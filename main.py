3066f4cb736bd2b44f23188c1f63622105e0a116
Kadir Can <kdrcn.uzmcu@gmail.com>
2023-06-28 18:07:12 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data_file_path = "128.turkish/restaurants_train_turkish.xml.dat"
-
-with open(data_file_path, "r", encoding="utf-8") as file:
-    text = file.read()
-    file.close()
-
-data = pd.Series(text)
-data.str.split("\n").tolist()
